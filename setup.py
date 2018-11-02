a582c06d60198750c95915be32d8f0aefdb12e1c
Lilitz <lilit.zargaryan@deloitte-audit-analytics.com>
2018-11-02 20:01:38 +0400

    Project now compatible for deployment in PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='NumberToArmWords',
+  packages=['NumberToArmWords'],
+  version='1.0',
+  description='This tool converts given number into words in Armenian language.',
+  author='Zargaryan Lilit',
+  author_email='zargaryan.lilit@gmail.com',
+  url='https://github.com/zargaryanlilit/NumberToArmWords', 
+  download_url= 'https://github.com/zargaryanlilit/NumberToArmWords.git',
+  keywords = ['Armenian numbers', 'Number to words'],
+  classifiers = [],
+)
